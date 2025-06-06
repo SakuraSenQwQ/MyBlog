c465289a3281bcd6a29ff3008f9e6b8f87dc621f
sakurasen <2402147211@qq.com>
2025-06-07 00:39:48 +0800

    On branch main

---
+++ obs_sctipts/post.js
@@ -0,0 +1,100 @@
+const util = require('util');
+
+const child_process = require('child_process');
+
+const exec = util.promisify(child_process.exec);
+
+
+
+function getCreateTimeAsFileName() {
+
+    var d = new Date();
+
+    var year = d.getFullYear();
+
+    function month() {
+        const m = JSON.stringify(d.getMonth())
+        if (m.length === 1) {
+            return "0" + (d.getMonth() + 1)
+        } else {
+            return d.getMonth() + 1
+        }
+    }
+    function day() {
+        const m = JSON.stringify(d.getDate())
+        if (m.length === 1) {
+            return "0" + d.getDate()
+        } else {
+            return d.getDate()
+        }
+    }
+    function hour() {
+        const m = JSON.stringify(d.getHours())
+        if (m.length === 1) {
+            return "0" + d.getHours()
+        } else {
+            return d.getHours()
+        }
+    }
+    function minute() {
+        const m = JSON.stringify(d.getMinutes())
+        if (m.length === 1) {
+            return "0" + d.getMinutes()
+        } else {
+            return d.getMinutes()
+        }
+    }
+    function second() {
+        const m = JSON.stringify(d.getSeconds())
+        if (m.length === 1) {
+            return "0" + d.getSeconds()
+        } else {
+            return d.getSeconds()
+        }
+    }
+
+    var time = year + month() + day() + hour() + minute() + second();
+
+    return time;
+
+}
+
+
+
+
+
+// execute command function
+
+
+
+async function executeCommand() {
+
+    const fileName = "index.md";
+
+    const { stdout, stderr } = await exec('npm run new-post -- ' + getCreateTimeAsFileName() + '/' + fileName, { cwd: app.fileManager.vault.adapter.basePath });
+
+    console.log('stdout:', stdout);
+
+    console.log('stderr:', stderr);
+
+    if (stdout) {
+
+        new Notice("New Blog Created[" + fileName + "]")
+
+    } else {
+
+        new Notice("New Blog Create Faild. " + stderr)
+
+    }
+
+}
+
+
+
+
+
+module.exports = async function (context, req) {
+
+    await executeCommand();
+
+}
---
+++ src/content/spec/about.md
@@ -0,0 +1,40 @@
+# 关于
+# 我？
+更多信息都写在主页了，不做赘述
+https://sakurasen.cn
+# 使用素材
+除文章外所有使用的素材出处
+## 头像
+[#明日方舟 明日方舟头像8 - QuAn\_的插画 - pixiv](https://www.pixiv.net/artworks/93388262)
+## 背景
+[#明日方舟 画了阿米娅和罗德岛 - 法师来自未来的插画 - pixiv](https://www.pixiv.net/artworks/124383634)
+## 主题
+[GitHub - saicaca/fuwari: ✨A static blog template built with Astro.](https://github.com/saicaca/fuwari)
+
+
+# 友情链接
+[SakuraSen'sBlog](https://sakurasen.cn) 无限进步
+
+
+
+
+## 申请友链
+
+:::tip[如何申请]
+ 如需申请，请写邮件至im@sakurasen.cn
+:::
+
+**条件**
+
+请先将我站加入贵站友链后申请
+
+✅ HTTPS支持
+
+✅ 无违规内容/符合社会主义核心价值观
+
+✅ 网站类型为个人博客，无企业/商业用途
+
+- 站名：森语
+- 域名：https://sakurasen.cn
+- 描述：无限进步
+
