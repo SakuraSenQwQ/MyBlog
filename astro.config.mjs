d1a70b733acb6038688bc5e82fa7b1716843a62a
sakurasen <2402147211@qq.com>
2025-06-08 18:17:53 +0800

    fix siteurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 import rehypeExternalLinks from 'rehype-external-links';
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://blog.sakurasen.cn/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
